1bb62e95d98a6d9d7a9b443c5004071138f28dad
Adriano <adriano_alves_2008@hotmail.com>
2022-07-14 10:22:26 -0300

    primeiro commit

---
+++ assets/js/slides.js
@@ -0,0 +1,90 @@
+'use strict'
+
+const slideWrapper = document.querySelector('[data-slide="wrapper"]')
+const slideList = document.querySelector('[data-slide="list"]')
+const navPreviousButton = document.querySelector(
+  '[data-slide="nav-previous-button"]'
+)
+const navNextButton = document.querySelector('[data-slide="nav-next-button"]')
+const ControlsWrapper = document.querySelector(
+  '[data-slide="controls-wrapper"]'
+)
+const slideItems = document.querySelectorAll('[data-slide="item"]')
+const controlButtons = document.querySelectorAll(
+  '[data-slide="control-button"]'
+)
+
+const state = {
+  startingPoint: 0,
+  savedPosition: 0,
+  currentPoint: 0,
+  movement: 0,
+  currentSlideIndex: 0
+}
+
+function translateSlide({ position }) {
+  state.savedPosition = position
+  slideList.style.transform = `translateX(${position}px)`
+}
+
+function setVisibleSlide({ index }) {
+  const slideItem = slideItems[index]
+  const slideWidth = slideItem.clientWidth
+  const windowWidth = document.body.clientWidth
+  const margin = (windowWidth - slideWidth) / 2
+  const position = margin - index * slideWidth
+  state.currentSlideIndex = index
+  translateSlide({ position: position })
+}
+
+function nextSlide() {
+  setVisibleSlide({ index: state.currentSlideIndex + 1 })
+}
+
+function previousSlide() {
+  setVisibleSlide({ index: state.currentSlideIndex - 1 })
+}
+
+function onMouseDown(event, index) {
+  const slideItem = event.currentTarget
+  state.startingPoint = event.clientX
+  state.currentPoint = event.clientX - state.savedPosition
+  state.currentSlideIndex = index
+  slideItem.addEventListener('mousemove', onMouseMove)
+}
+
+function onMouseMove(event) {
+  state.movement = event.clientX - state.startingPoint
+  const position = event.clientX - state.currentPoint
+  translateSlide({ position: position })
+}
+
+function onMouseUp(event) {
+  const slideItem = event.currentTarget
+  const slideWidth = slideItem.clientWidth
+
+  if (state.movement < -150) {
+    nextSlide()
+  } else if (state.movement > 150) {
+    previousSlide()
+  } else {
+    setVisibleSlide({ index: state.currentSlideIndex })
+  }
+
+  slideItem.removeEventListener('mousemove', onMouseMove)
+}
+
+slideItems.forEach(function (slideItem, index) {
+  slideItem.addEventListener('dragstart', function (event) {
+    event.preventDefault()
+  })
+
+  slideItem.addEventListener('mousedown', function (event) {
+    onMouseDown(event, index)
+  })
+
+  slideItem.addEventListener('mouseup', onMouseUp)
+})
+
+navNextButton.addEventListener('click', nextSlide)
+navPreviousButton.addEventListener('click', previousSlide)
